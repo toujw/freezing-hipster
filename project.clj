03f277c4ba10749cce61d10168c82a354f3a2a2e
John Touron <johntouron@gmail.com>
2013-05-30 00:46:47 +0100

    Added main functionality.

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojure-csv/clojure-csv "2.0.0-alpha2"]
+                 [joda-time "2.2"]
+                 [ororo "0.1.0"]]
   :main freezing-hipster.core)
--- src/freezing_hipster/core.clj
+++ src/freezing_hipster/core.clj
@@ -1,9 +1,49 @@
 (ns freezing-hipster.core
+  (:use [ororo.core :only [history]]
+        [clojure-csv.core :only [write-csv]])
+  (:import [org.joda.time DateTime DateTimeComparator]
+           [org.joda.time.format DateTimeFormat])
   (:gen-class))
 
+(defn- make-csv-row
+  "Creates a CSV row suitable for output from the raw history data."
+  [raw-hist]
+  (let [{:keys [date maxhumidity minhumidity maxtempi mintempi meanpressurei]}
+        (first (:dailysummary raw-hist))]
+    [(str (:mon date) "/" (:mday date) "/" (:year date))
+     maxhumidity
+     minhumidity
+     maxtempi
+     mintempi
+     meanpressurei]))
+
+(defn- print-usage
+  "Prints application usage."
+  []
+  (println "Usage: java -jar freezing-hipster.jar KEY LOCATION FROM-DATE TO-DATE OUT-FILE"))
+
+(defn- date-seq
+  "Returns a sequence of joda time objects between from and to (inclusive)."
+  [from to]
+  (let [cmp (DateTimeComparator/getDateOnlyInstance)]
+    (if (> (.compare cmp from to) 0)
+      (throw (IllegalArgumentException. "The from date must be earlier than the to date."))
+      (take-while #(<= (.compare cmp % to) 0)
+                  (iterate #(.plusDays ^DateTime % 1) from)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Gets history data and writes it to a CSV file."
   [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (try
+    (if (not= (count args) 5)
+      (print-usage)
+      (let [[key loc from to file] args
+            formatter (DateTimeFormat/forPattern "YYYYMMdd")
+            [from to] [(.parseDateTime formatter from) (.parseDateTime formatter to)]
+            rows (->> (date-seq from to)
+                      (map #(.toString ^DateTime % "YYYYMMdd"))
+                      (map #(history key loc %))
+                      (map make-csv-row))]
+        (spit file (write-csv rows))))
+    (catch IllegalArgumentException e
+      (println (.getMessage e)))))
