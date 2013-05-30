59fa5c1fa9c6ca9fbe2c0d21af20860e7b438eb0
John Touron <johntouron@gmail.com>
2013-05-30 01:12:53 +0100

    Incremented version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject freezing-hipster "0.1.0-SNAPSHOT"
+(defproject freezing-hipster "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
